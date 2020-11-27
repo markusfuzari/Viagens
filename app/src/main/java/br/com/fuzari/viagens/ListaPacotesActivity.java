1d1390c6f2d4e5c404e607a28b8e5eef312faca0
Markus Fuzari <markusfuzari@gmail.com>
2020-11-26 23:05:09 -0300

    Iniciando projeto com layout android

---
+++ app/src/main/java/br/com/fuzari/viagens/ListaPacotesActivity.java
@@ -0,0 +1,14 @@
+package br.com.fuzari.viagens;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ListaPacotesActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_lista_pacotes);
+    }
+}
